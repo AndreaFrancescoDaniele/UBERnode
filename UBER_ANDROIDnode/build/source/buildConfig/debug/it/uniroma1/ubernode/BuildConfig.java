4eefc0ec7bd4c12a34e60063d634c793a7705adc
AndreaFrancescoDaniele <andrea.francesco.daniele@gmail.com>
2013-11-14 13:13:35 +0100

    ROSnode+ANDROIDnode

--- UBER_ANDROIDnode/build/source/buildConfig/debug/it/uniroma1/ubernode/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package it.uniroma1.ubernode;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
